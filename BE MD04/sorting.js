09255e20e889b81b1586bb5a8f7d674d9aa014d1
Himanshu Kumar Singh <hk5348@gmail.com>
2022-07-29 03:10:35 +0530

    js

--- BE MD04/sorting.js
+++ BE MD04/sorting.js
@@ -11,4 +11,34 @@ let reverseArray= tempArray.reverse();
 console.log(sortedArray);
 console.log(reverseArray);
 
-//agle module mein dekh solution sahi se!
+//Sorting in Ascending order:
+
+function sorta(arr){
+    for(i=0;i<arr.length;i++){
+        if(arr[i]>arr[i+1]){ //Changung the sign will change the order of display for the array.
+            let temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+        }
+    }for(i=0;i<arr.length;i++){
+        if(arr[i]>arr[i+1]) return sorta(arr);
+    }return arr;
+}
+
+//Sorting in descending order:
+function sortd(arr){
+    for(i=0;i<arr.length;i++){
+        if(arr[i]<arr[i+1]){ //Changing the sign will change the order of display for the array.
+            let temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+        }
+    }for(i=0;i<arr.length;i++){
+        if(arr[i]<arr[i+1]) return sortd(arr);
+    }return arr;
+}
+
+//Testing:
+let set=[23,65,34,88,12,-1,-234,11,56];
+console.log(sorta(set));
+console.log(sortd(set));
